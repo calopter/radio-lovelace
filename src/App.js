4b21afed8d0a4d57dd2456ea874f661ba54863ce
Mira <calopter@gmail.com>
2019-11-29 19:43:42 -0800

    SWITCH action

--- src/App.js
+++ src/App.js
@@ -63,12 +63,42 @@ const top = (tracks, action) => {
   }
 };
 
+const switchTrack = (tracks, action) => {
+  const i = tracks[action.side].findIndex(track => track.id === action.id);
+
+  let newFrom = [...tracks[action.side]];
+  let newTo = [];
+  
+  newFrom.splice(i, 1);
+  
+  if (action.side === 'morningTracks') {
+    newTo = [...tracks.eveningTracks];
+    newTo.splice(i, 0, tracks.morningTracks[i]);
+    
+    return {
+      morningTracks: newFrom,
+      eveningTracks: newTo
+    };
+  }
+  else {
+    newTo = [...tracks.morningTracks];
+    newTo.splice(i, 0, tracks.eveningTracks[i]);
+    
+    return {
+      morningTracks: newTo,
+      eveningTracks: newFrom
+    };
+  }
+}
+
 const tracksReducer = (tracks, action) => {
   switch(action.type) {
     case 'FAVE':
       return fave(tracks, action);
     case 'TOP':
       return top(tracks, action);
+    case 'SWITCH':
+      return switchTrack(tracks, action);
     default:
       return playlists;
   }
